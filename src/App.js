494dc2d7b67cdefe678beff72eb177039b4c985c
Johnstone Marucha <marucha.webdev@yahoo.com>
2022-11-07 21:46:15 +0300

    Add Tailwind CSS, install React Router

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-zinc-800 h-screen text-neutral-200">
+     <h1 className='text-3xl font-mono '>Ozymandias Book Reviewer</h1>
     </div>
   );
 }

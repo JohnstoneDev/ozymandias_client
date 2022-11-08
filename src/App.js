134515e20e1d22a14f9abcd98c3bc076965680fb
Johnstone Marucha <marucha.webdev@yahoo.com>
2022-11-08 07:47:16 +0300

    Configure REact Router, Add json file for a sample database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import MainDisplay from './Components/MainDisplay';
 
 function App() {
   return (
-    <div className="App bg-zinc-800 h-screen text-neutral-200">
-     <h1 className='text-3xl font-mono '>Ozymandias Book Reviewer</h1>
+    <div className="App  bg-zinc-800 h-screen text-neutral-200 font-mono">
+     <h1 className='text-3xl'>Ozymandias Book Reviewer</h1>
+      <MainDisplay />
     </div>
   );
 }

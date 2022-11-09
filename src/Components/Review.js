9c2822b620ac356ef1838c6832858d9dea3969e8
Johnstone Marucha <marucha.webdev@yahoo.com>
2022-11-09 22:27:53 +0300

    Update review display

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,8 +1,18 @@
 // Review Component should receive a prop of a review item from review list and display it
 
-function Review(){
+function Review( { review }){
+    const { title, comment } = review 
     return (
         <>
+            <div>
+                <h3>Book Title : {title}</h3>
+                <div className="flex flex-row gap-4 align-middle justify-center">
+                <h3>Comments :</h3>
+                <p>
+                    {comment}
+                </p>
+                </div>
+            </div>
         </>
     )
 }
--- src/Components/ReviewsList.js
+++ src/Components/ReviewsList.js
@@ -1,10 +1,13 @@
 // Review List component should render individual review items
 import Review from "./Review";
 
-function ReviewList(){
+function ReviewList({ reviews }){
     return (
         <div className="h-screen">
-            Render Review Items here
+            {reviews.length <=0 ? <h3>There are no reviews!</h3> : <h3> This are your reviews </h3>}
+            {reviews.map(review => {
+                return <Review key={review.id} review={review} />
+                })}
         </div>
     )
 }
